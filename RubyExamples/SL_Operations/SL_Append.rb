90d95675eeb9303716f0deeb7d62fb0b148a96f3
Sancarn <sancarn@hotmail.co.uk>
2017-11-16 15:40:38 +0000

    Create SL_Append.rb

---
+++ RubyExamples/SL_Operations/SL_Append.rb
@@ -0,0 +1,5 @@
+localStorage = WSLocalStorage.new("a8344089-9f06-4058-9955-57283c090659")
+if localStorage[:id]
+  WSApplication.load_selection localStorage[:id]
+  WSApplication.save_selection localStorage[:id]
+end
