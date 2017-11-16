9896d1f132b79328204b4ac34344c940f5b8f646
Sancarn <sancarn@hotmail.co.uk>
2017-11-16 15:41:24 +0000

    Create SL_Clear.rb

---
+++ RubyExamples/SL_Operations/SL_Clear.rb
@@ -0,0 +1,6 @@
+localStorage = WSLocalStorage.new("a8344089-9f06-4058-9955-57283c090659")
+if localStorage[:id]
+  net = WSApplication.current_network
+  net.clear_selection
+  net.save_selection localStorage[:id]
+end
