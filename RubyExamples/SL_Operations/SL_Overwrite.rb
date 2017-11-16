9bfcc4b217ca9b93f146f61859f1480a2e4887cc
Sancarn <sancarn@hotmail.co.uk>
2017-11-16 15:43:43 +0000

    Update SL_Overwrite.rb

--- RubyExamples/SL_Operations/SL_Overwrite.rb
+++ RubyExamples/SL_Operations/SL_Overwrite.rb
@@ -1,3 +1,4 @@
+require('libs\WSLocalStorage.rb')
 localStorage = WSLocalStorage.new("a8344089-9f06-4058-9955-57283c090659")
 if localStorage[:id]
   WSApplication.current_network.save_selection localStorage[:id]
